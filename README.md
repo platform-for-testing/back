545367a0fabd0a547222ab67f9b7a12fb7c3a4fb
PavelMelnyk <pavlik1.618@gmail.com>
2017-09-24 11:40:11 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # back
+
+## Dev env
+
+```
+npm i
+```
+
+### Local db
+
+run mongodb from container https://hub.docker.com/_/mongo/
+`docker ps` - show runing containers
+`docker ps -a` - show all containers
+`docker images` - show images
+`docker exec -it {container id} bash` - connect to container bash
+
+
+```
+docker pull mongo
+docker run -p 27017:27017 --name some-mongo -d mongo
+
+docker exec -it 01d bash
+```
---
+++ index.js
@@ -0,0 +1,28 @@
+const Koa = require('koa');
+const app = module.exports = new Koa();
+const MongoClient = require('mongodb').MongoClient; 
+
+const url = 'mongodb://localhost:27017/test';
+
+MongoClient.connect(url, function(err, db) {
+  console.log('err', err);
+  console.log("Connected correctly to server.");
+  // db.close();
+
+  db.collection('restaurants').insertOne({
+    name: 'test'
+  }, (err, res) => {
+    console.log({err, res});
+    app.listen(3000);
+  });
+
+  
+});
+
+
+
+app.use(async function(ctx) {
+  ctx.body = 'Hello World';
+});
+
+// if (!module.parent) app.listen(3000);
