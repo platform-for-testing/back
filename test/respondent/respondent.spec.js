214b40346ed3d39df8e03960d3e07fd3de7101a9
normatovv <normatovv@edi.su>
2017-10-20 15:52:15 +0300

    added test

--- test/respondent/respondent.spec.js
+++ test/respondent/respondent.spec.js
@@ -3,7 +3,7 @@ const {initHelper, cleanHelper} = require('./helper');
 const assert = require('assert');
 const superTest = require('supertest');
 const fs = require('fs');
-const respondentSample =JSON.parse( fs.readFileSync('test/respondent/respondent-test-data.json', 'utf8'));
+const respondentSample = JSON.parse(fs.readFileSync('test/respondent/respondent-test-data.json', 'utf8'));
 
 require('should');
 
@@ -15,7 +15,6 @@ describe('Respondents', function () {
         await initHelper();
         pftInstance = new PftServer();
         await pftInstance.start();
-
         request = superTest(pftInstance.server);
     });
 
@@ -28,13 +27,13 @@ describe('Respondents', function () {
         describe('Respondents get', () => {
             it('should send code 200', async () => {
                 await request
-                    .get('/respondents')
+                    .get('/respondent')
                     .set('Accept', 'application/json')
                     .expect(200);
             });
             it('should send 3 objects of respondents', async () => {
                 await request
-                    .get('/respondents')
+                    .get('/respondent')
                     .set('Accept', 'application/json')
                     .expect(200)
                     .then(response => {
@@ -43,7 +42,7 @@ describe('Respondents', function () {
             });
             it('should get respondents which equal sample', async () => {
                 return request
-                    .get('/respondents')
+                    .get('/respondent')
                     .set('Accept', 'application/json')
                     .expect(200)
                     .then(response => {
@@ -52,12 +51,34 @@ describe('Respondents', function () {
                         assert.deepEqual(respondent, respondentSample);
                     })
             });
+
+            it('should get respondent by id', async () => {
+                let id;
+                await request
+                    .post('/respondent')
+                    .send(respondentSample)
+                    .expect(200)
+                    .then(response => {
+                        let respondent = (response.body.slice(0, 1))[0];
+                        id = respondent['_id'];
+                    });
+                console.log('id=' + id);
+                return request
+                    .get('/respondent/' + id)
+                    .set('Accept', 'application/json')
+                    .expect(200)
+                    .then(response => {
+                        let respondent = (response.body.slice(0, 1))[0];
+                        delete respondent['_id'];
+                        assert.deepEqual(respondent, respondentSample);
+                    });
+            });
         });
         describe('Respondents post', () => {
 
             it('should return status code 400 if object not valid', async () => {
                 return request
-                    .post('/respondents')
+                    .post('/respondent')
                     .send({
                         name: 'qwe'
                     })
@@ -67,18 +88,18 @@ describe('Respondents', function () {
 
             it('should return status code 200 if object valid', async () => {
                 return request
-                    .post('/respondents')
+                    .post('/respondent')
                     .send(respondentSample)
                     .expect(200);
             });
 
             it('should send 4 objects of respondents', async () => {
                 return request
-                    .get('/respondents')
+                    .get('/respondent')
                     .expect('Content-Type', /json/)
                     .expect(200)
                     .then(response => {
-                        assert.equal(response.body.length, 4);
+                        assert.equal(response.body.length, 5);
                     });
             });
         });
