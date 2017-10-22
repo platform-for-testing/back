41880bf5c0d4a143e3c6506eadfb3089542008e6
Morozov Viacheslav <morozovvjacheslav@gmail.com>
2017-10-22 11:22:27 +0300

    Create helper for POST (all activations)

---
+++ test/activation/helper.js
@@ -0,0 +1,37 @@
+const Db = require('../../lib/db/index');
+const config = require('../../lib/config');
+const bunyan = require('bunyan');
+const logger = bunyan.createLogger({
+    name: config.get('helper'),
+    level: config.get('loggerLevel')
+});
+const collectionName = "Activations";
+
+const db = new Db(config, logger);
+
+async function initHelper() {
+    await db.init();
+    await setupActivation();
+    await  db.closeConnection();
+}
+
+async function cleanHelper() {
+    await db.init();
+    await removeCollection();
+    await  db.closeConnection();
+}
+
+async function removeActivation() {
+    logger.info('removeActivation');
+    db.removeCollection(collectionName);
+}
+
+async function setupActivation() {
+    logger.info('setupActivation');
+    db.create(collectionName, collectionJson);
+}
+
+module.exports = {
+    initHelper,
+    cleanHelper
+};
