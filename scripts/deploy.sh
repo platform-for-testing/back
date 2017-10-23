50a596890eda03bf523a5b21c1df2ab76e1b0567
Vitaliy Normatov <vnormatov@gmail.com>
2017-10-23 21:13:53 +0300

    v22

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-ssh $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH/deploy.sh  -o StrictHostKeyChecking=no
+ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH/deploy.sh
 scp package.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
