4f05b3a6e1d2527290c809afc4bd36403092e7d0
normatovv <normatovv@edi.su>
2017-10-23 18:06:05 +0300

    added install v15

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+mkdir build
+rsync -av --progress $TRAVIS_BUILD_DIR $TRAVIS_BUILD_DIR/build --exclude build
+tar -czf package.tgz build
 scp package.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
 ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/deploy.sh
