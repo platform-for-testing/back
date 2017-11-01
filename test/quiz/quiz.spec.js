a4fa7862b655b9969760ab809235522c1d71003a
Vitaliy <biggone24@gmail.com>
2017-11-01 09:39:51 +0200

    01.11.2017 commit

--- test/quiz/quiz.spec.js
+++ test/quiz/quiz.spec.js
@@ -4,6 +4,7 @@ require('should');
 
 const PftServer = require('../../lib/index');
 const { quizOne, quizTwo, questionTwo } = require('./quiz-test-data');
+const deleteIdSample = "59f597d73aa21a10ac0ddcbd";
 
 describe.only('Quiz', () => {
 	let request;
@@ -86,4 +87,54 @@ describe.only('Quiz', () => {
 				.expect(400);
 		});
 	});
+describe('DELETE /tests', () => {
+    /*it('should get quiz by id', async() => {
+        let id;
+        await request
+            .post('/respondent')
+            .send(respondentSample)
+            .expect(200)
+            .then(response => {
+                let respondent = (response.body.slice(0, 1))[0];
+                id = respondent['_id'];
+            });
+        return request
+            .get('/respondent/' + id)
+            .set('Accept', 'application/json')
+            .expect(200)
+            .then(response => {
+                let respondent = (response.body.slice(0, 1))[0];
+                delete respondent['_id'];
+                assert.deepEqual(respondent, respondentSample);
+            });
+    });*/
+
+	it('should return deleted quiz', async () => {
+		// arrange
+
+		//act
+		await request
+			.delete('/tests')
+			.send("59f597d73aa21a10ac0ddcbd")
+			.expect(200)
+			.then(response => {
+				let quiz = (response.body.slice(0, 1))[0];
+				id = quiz['_id'];
+			});
+		return request
+			.get('/tests/' + id)
+			.set('Accept', 'application/json')
+			.expect(200)
+			.then(response => {
+				let test = (response.body.slice(0, 1))[0];
+				delete quiz['_id'];
+				assert.deepEquel(quiz, deleteIdSample);
+			});
+	});
 });
+});
+
+
+
+
+
