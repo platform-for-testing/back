36d05180402459e4b55c6487493c8432ddf4642e
normatovv <normatovv@edi.su>
2017-11-02 12:27:41 +0200

    refactored

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,6 +10,5 @@ rm -rf /home/deploy/back-dev/serv
 mkdir /home/deploy/back-dev/serv
 rsync -av * /home/deploy/back-dev/serv --exclude=.git --exclude=test
 rm -rf /home/deploy/back-dev/package/build
-export MONGODB_URI=mongodb://backuser:backPass@ds127375.mlab.com:27375/testplatformdev
 cd /home/deploy/back-dev/serv
 npm run start:dev
