879ffa09a0248b202f4bba3f1e0e5b80c081e831
normatovv <normatovv@edi.su>
2017-11-02 12:15:44 +0200

    refactored

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,15 @@
-#!/usr/bin/env bash
-ssh deploy@95.85.54.211 sh /home/deploy/back-dev/package/deploy.sh
+#!/bin/bash
+cd /home/deploy/back-dev/serv
+npm stop
+sleep 10
+cd /home/deploy/back-dev/package
+tar -xzf package.tgz
+rm package.tgz
+cd /home/deploy/back-dev/package/build/back
+rm -rf /home/deploy/back-dev/serv
+mkdir /home/deploy/back-dev/serv
+rsync -av * /home/deploy/back-dev/serv --exclude=.git --exclude=test
+rm -rf /home/deploy/back-dev/package/build
+export MONGODB_URI=mongodb://backuser:backPass@ds127375.mlab.com:27375/testplatformdev
+cd /home/deploy/back-dev/serv
+npm run start:dev
---
+++ scripts/startDeploy.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+ssh deploy@95.85.54.211 sh /home/deploy/back-dev/package/deploy.sh
+
