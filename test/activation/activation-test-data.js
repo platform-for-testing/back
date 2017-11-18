ada56921f4163601204876d6ee5bfa4559b2c6c6
Vitaliy Normatov <vnormatov@gmail.com>
2017-11-18 11:17:15 +0200

    fixed activation tests

---
+++ test/activation/activation-test-data.js
@@ -0,0 +1,11 @@
+const activationOne = {
+	respondentsIds: 'c2bsab3c-f83d-4870-a351-429198c4ead1',
+	shareLink: 'http://link',
+	quize: 'quizName',
+	activationOptions: 'c2bsab3c-f83d-4870-a351-429198c4ead2',
+};
+
+module.exports = {
+	activationOne,
+};
+
