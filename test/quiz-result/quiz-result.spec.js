4a664e1fcd5bcb8a134a7d9d2e96748cbb2c8d92
Yurii Holiuk <yuriiholiuk@gmail.com>
2017-11-26 01:20:12 +0200

    working on tests

--- test/quiz-result/quiz-result.spec.js
+++ test/quiz-result/quiz-result.spec.js
@@ -1,15 +1,16 @@
 const PftServer = require('../../lib/index');
-// const assert = require('assert');
+const assert = require('assert');
 const superTest = require('supertest');
 const { activationOne } = require('../activation/activation-test-data');
-// const { userResultMock } = require('./user-result.mock');
+const { successResult, invalidResultOne, invalidResultTwo } = require('./user-result.mock');
 
 require('should');
 
-describe('QuizResult', () => {
+describe.only('QuizResult', () => {
 	let request;
 	let pftInstance;
 	let token;
+	let activationId;
 
 	before(async () => {
 		pftInstance = new PftServer();
@@ -24,10 +25,13 @@ describe('QuizResult', () => {
 
 		token = response.body.token;
 
-		await request
+		const activationResponse = await request
 			.post('/admin/activations')
 			.set('Authorization', `Bearer ${token}`)
 			.send(activationOne);
+
+		activationId = activationResponse.body.id;
+		console.log(activationId);
 	});
 
 	afterEach(async () => {
@@ -35,12 +39,62 @@ describe('QuizResult', () => {
 	});
 
 	after(async () => {
-		await pftInstance.db.removeCollection('activations');
-		// await pftInstance.db.removeCollection('users');
+		// await pftInstance.db.removeCollection('activations');
+		await pftInstance.db.removeCollection('users');
 		await pftInstance.stop();
 	});
 
-	describe('/respondent/test/:activationId', async () => {
+	describe('POST /respondent/test/:activationId', async () => {
+		afterEach(async () => {
+			await pftInstance.db.removeCollection('quizresults');
+		});
 
+		it('should return success if correct data passed', async () => {
+			const response = await request
+				.post(`/respondent/test/${activationId}`)
+				// .set('Authorization', `Bearer ${token}`)
+				.set('Accept', 'application/json')
+				.send(successResult)
+				.expect(200);
+
+			assert.equal(response.body.success, true);
+		});
+
+		it('should return 400 if some ids are incorrect', async () => {
+			await request
+				.post(`/respondent/test/${activationId}`)
+				// .set('Authorization', `Bearer ${token}`)
+				.set('Accept', 'application/json')
+				.send(invalidResultOne)
+				.expect(400);
+		});
+
+		it('should return 400 if no answers presented', async () => {
+			await request
+				.post(`/respondent/test/${activationId}`)
+				// .set('Authorization', `Bearer ${token}`)
+				.set('Accept', 'application/json')
+				.send(invalidResultTwo)
+				.expect(400);
+		});
+
+		it('should create QuizResult with score 100', async () => {
+			await request
+				.post(`/respondent/test/${activationId}`)
+				// .set('Authorization', `Bearer ${token}`)
+				.set('Accept', 'application/json')
+				.send(successResult)
+				.expect(200);
+
+			const response = await request
+				.get('/admin/results')
+				.set('Authorization', `Bearer ${token}`)
+				.set('Accept', 'application/json')
+				.expect(200);
+
+			const result = response.body[0];
+
+			assert.equal(result.meta.score, 100);
+		});
 	});
 });
