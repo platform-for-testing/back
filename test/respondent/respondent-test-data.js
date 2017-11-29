3700aa51e63a48a9856d6d19224de2c0c63d0093
Alex <odmitrenko1@gmail.com>
2017-11-29 23:18:44 +0200

    Returned respondent joi validation.

--- test/respondent/respondent-test-data.js
+++ test/respondent/respondent-test-data.js
@@ -1,6 +1,8 @@
 const respondentOne = {
 
 	email: 'wqsswe@qwxxsse.com',
+	firstName: 'John',
+	lastName: 'Doe',
 	imageURL: 'sdfsssd',
 	testResultsIds: ['ssssd'],
 	activationsIds: ['c2bsab3c-f83d-4870-a351-429198c4ead1'],
